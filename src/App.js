502e2b7398e82a123016493c33574fd42a5020e9
Alex <iordan.alexandru97@yahoo.com>
2022-05-27 08:03:29 +0200

    integratedClarifai

--- src/App.js
+++ src/App.js
@@ -6,7 +6,34 @@ import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Particless from './components/particles/Particless';
+
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '9b2d44293f9647a1a8967db60290b4a5'
+})
+  
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onSubmit = () =>{
+    console.log('click');
+    app.models.predict('a403429f2ddf4b49b307e318f00e528b','https://www.faceapp.com/static/img/content/compare/beard-example-before@3x.jpg').then(
+      function(response){
+        console.log(response);
+      },
+      function(err){
+
+      }
+    )
+  }
   render () {
     return(
       <div>
@@ -14,7 +41,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm/>
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
 
         {/* <FaceRecognition/> */}
       </div>
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -3,13 +3,13 @@ import './ImageLinkForm.css';
 
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onSubmit}) => {
     return (
        <div className='container'>
            <p className="f3" >{'This Magic Brain will detect faces in your pictures. Give it a try'}</p>
            <div className='central-items form pa4 br3 shadow-5'>
-               <input className="f4 pa2 w-70 center" type='text'/>
-               <button className="w-30 grow f4 link ph3 pv2 dib white glow-on-hover">Detect</button>
+               <input className="f4 pa2 w-70 center" type='text' onChange={onInputChange}/>
+               <button className="w-30 grow f4 link ph3 pv2 dib white glow-on-hover" onClick={onSubmit}>Detect</button>
            </div>
        </div>
 
