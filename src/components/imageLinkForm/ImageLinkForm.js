3d04fce60e953e8ebd56608937caf29759abc05c
Alex <iordan.alexandru97@yahoo.com>
2022-06-07 09:05:00 +0200

    final update

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -4,7 +4,8 @@ import React from 'react';
 
 
 const ImageLinkForm = ({onInputChange, onButtonAction, box}) => {
-    let flag = box.length > 0 ? 'Total of '+box.length+' faces' : '';
+    let flag = box.length > 0 ? 
+        <div>Total of {box.length} faces. <div style={{color:'green', display:'inline', fontWeight:'bold'}}>Green</div> frame is the highest probability a face was detected, <div style={{color:'red', display:'inline', fontWeight:'bold'}}>Red</div> frame is the lowest</div> : '';
     return (
        <div className='container'>
            <p className="f3" >{'This Magic Brain will detect faces in your pictures. Give it a try'}</p>
@@ -12,7 +13,7 @@ const ImageLinkForm = ({onInputChange, onButtonAction, box}) => {
                     <input className="f4 pa2 w-70 center chenar" type='text' onChange={onInputChange} placeholder={'url of a jpg image'}/>
                     <button className="w-30 grow f4 link ph3 pv2 dib white glow-on-hover " onClick={onButtonAction}>Detect</button>
                 </div>
-            <div className='center'>{flag}</div>
+            <div className='center'>{flag} </div>
        </div>
 
     )
